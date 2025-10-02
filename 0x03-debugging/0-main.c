741d6b50876ed2c737d8432e38582b62cdc8512c
aelbouss <elboussailianass37@gmail.com>
2025-10-03 00:56:26 +0100

    done 3

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/*
+ * a code that will test positive_negative function if it works correct or not
+ */
+
+int	main(void)
+{
+	int	test;
+
+	test = 0;
+	positive_negative(test);
+	return (0);
+}
